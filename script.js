50a32ba724ba5e7c6aabb46cfb54cb932631f2de
Austin <agobby471@gmail.com>
2025-09-04 22:07:54 -0400

    Added ability to add books to table

--- script.js
+++ script.js
@@ -11,35 +11,53 @@ function addBookToLibrary(name, author, pages){
     const book = new Book(crypto.randomUUID(), name, author, pages);
 
     myLibrary.push(book);
+
+    createTable();
 }
 
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkein', 295);
 addBookToLibrary('The Hobbit 2', 'J.R.R. Tolkein', 268);
 addBookToLibrary('The Hobbit 3', 'J.R.R. Tolkein', 318);
 
-const table = document.getElementById("library");
-console.log(table);
+function createTable(){
+    const table = document.getElementById("library");
 
-myLibrary.forEach(element => {
-    const tableRow = document.createElement("tr");
-    
-    const itemOne = document.createElement("td");
-    itemOne.textContent = element.id;
-    tableRow.appendChild(itemOne);
+    while(table.children.length != 1){
+        if(table.children.length > 1){
+            table.removeChild(table.lastChild);
+        }
+    }
 
-    const itemTwo = document.createElement("td");
-    itemTwo.textContent = element.name;
-    tableRow.appendChild(itemTwo);
+    myLibrary.forEach(element => {
+        const tableRow = document.createElement("tr");
+        
+        const itemOne = document.createElement("td");
+        itemOne.textContent = element.id;
+        tableRow.appendChild(itemOne);
 
-    const itemThree = document.createElement("td");
-    itemThree.textContent = element.author;
-    tableRow.appendChild(itemThree);
+        const itemTwo = document.createElement("td");
+        itemTwo.textContent = element.name;
+        tableRow.appendChild(itemTwo);
 
-    const itemFour = document.createElement("td");
-    itemFour.textContent = element.pages;
-    tableRow.appendChild(itemFour);
+        const itemThree = document.createElement("td");
+        itemThree.textContent = element.author;
+        tableRow.appendChild(itemThree);
 
-    table.appendChild(tableRow);
-});
+        const itemFour = document.createElement("td");
+        itemFour.textContent = element.pages;
+        tableRow.appendChild(itemFour);
 
-//console.log(myLibrary[0].id);
+        table.appendChild(tableRow);
+    });
+
+}
+
+document.getElementById("form").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    addBookToLibrary(event.currentTarget.title.value, event.currentTarget.author.value, event.currentTarget.pages.value);
+
+    event.currentTarget.title.value = '';
+    event.currentTarget.author.value = '';
+    event.currentTarget.pages.value = '';
+})
